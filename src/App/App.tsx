3d26b5107b7004e5b9baee94e53400d52a2aeedc
Magomed-Amin954 <maruevmagomedamin@gmail.com>
2025-01-30 18:52:39 +0300

    сделал авторизацию

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,20 @@ import HomePage from "../pages/HomePage/HomePage";
 import "./App.scss";
 import LogIn from "../pages/login/LogIn";
 import Header from "../layout/Header/Header";
+import OrderListPage from "../pages/OrderListPage/OrderListPage";
 
 function App() {
   return (
     <div className="App">
       <div>
-          <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LogIn />} />
+          <Route path="/signin" element={<LogIn />} />
+          <Route path="/orders" element={<OrderListPage />} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </div>
   );
