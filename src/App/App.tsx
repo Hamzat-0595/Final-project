7f1cf1b0589d214086408cf3894f8f819b019f5d
Magomed-Amin954 <maruevmagomedamin@gmail.com>
2025-02-03 14:09:38 +0300

    сверстал

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,24 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import Footer from "../layout/Footer/Footer";
-import HomePage from "../pages/HomePage/HomePage";
+
 import "./App.scss";
-import LogIn from "../pages/login/LogIn";
-import Header from "../layout/Header/Header";
-import OrderListPage from "../pages/OrderListPage/OrderListPage";
+
+
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signin" element={<LogIn />} />
-          <Route path="/orders" element={<OrderListPage />} />
-        </Routes>
-        <Footer />
-      </div>
+        
     </div>
   );
 }
